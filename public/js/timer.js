bb4d1727cd299aac29d07bfac6ecdb2ea27e27ca
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-06-22 12:03:53 -0300

    First Init

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const title = document.querySelector("main section h2");
 
+  const title = document.querySelector("main section h2");
   title.innerHTML = "Timer"
+
+  
 });
